11aa64ee560209adcd9db7501d2d100343352916
Lorenfe <c-lorenfe@chronostep.com>
2023-10-25 18:10:56 +0800

    [UPDATE] LEVEL-10 - Admin dashboard

--- resources/views/pages/admin-auth/dashboard/index.blade.php
+++ resources/views/pages/admin-auth/dashboard/index.blade.php
@@ -1,161 +1,119 @@
 @extends('layouts.admin.app')
 
 @section('content')
-    <div class="container-fluid">
-        <h1 class="text-black-50">{{ __('words.SignInMessage') }}</h1>
-        <h1>Admin</h1>
-
-        <div class="dashboard">
-            <h2>Dashboard</h2>
-            <p>As of June 2023</p>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Parent's Counterpart</th>
-                        <th>Received</th>
-                        <th>Medical Share</th>
-                        <th>Total Earned</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>₱ 58,000.00</td>
-                        <td>₱ 18,500.00</td>
-                        <td>₱ 76,500.00</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-
-        <div class="col-lg-6">
-            <div class="card">
-                <div class="card-header border-0">
-                    <div class="d-flex justify-content-between">
-                        <h3 class="card-title">Sales</h3>
-                        <a href="javascript:void(0);">View Report</a>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="d-flex">
-                        <p class="d-flex flex-column">
-                            <span class="text-bold text-lg">$18,230.00</span>
-                            <span>Sales Over Time</span>
-                        </p>
-                        <p class="ml-auto d-flex flex-column text-right">
-                            <span class="text-success">
-                                <i class="fas fa-arrow-up"></i> 33.1%
-                            </span>
-                            <span class="text-muted">Since last month</span>
-                        </p>
-                    </div>
-
-                    <div class="position-relative mb-4">
-                        <div class="chartjs-size-monitor">
-                            <div class="chartjs-size-monitor-expand">
-                                <div class=""></div>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="dashboard">
+                <h2 class="text-left">Dashboard</h2>
+                <p class="text-left">As of June 2023</p>
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-md-3">
+                        <div class="info-box">
+                            <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">CPU Traffic</span>
+                                <span class="info-box-number">
+                                    10
+                                    <small>%</small>
+                                </span>
                             </div>
-                            <div class="chartjs-size-monitor-shrink">
-                                <div class=""></div>
-                            </div>
+
                         </div>
-                        <canvas id="sales-chart" height="235" style="display: block; height: 200px; width: 772px;"
-                            width="910" class="chartjs-render-monitor"></canvas>
+
                     </div>
-                    <div class="d-flex flex-row justify-content-end">
-                        <span class="mr-2">
-                            <i class="fas fa-square text-primary"></i> This year
-                        </span>
-                        <span>
-                            <i class="fas fa-square text-gray"></i> Last year
-                        </span>
+
+                    <div class="col-12 col-sm-6 col-md-3">
+                        <div class="info-box mb-3">
+                            <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Likes</span>
+                                <span class="info-box-number">41,410</span>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                    <div class="clearfix hidden-md-up"></div>
+                    <div class="col-12 col-sm-6 col-md-3">
+                        <div class="info-box mb-3">
+                            <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">Sales</span>
+                                <span class="info-box-number">760</span>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="col-12 col-sm-6 col-md-3">
+                        <div class="info-box mb-3">
+                            <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+                            <div class="info-box-content">
+                                <span class="info-box-text">New Members</span>
+                                <span class="info-box-number">2,000</span>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="card">
+                    <div class="card-header border-0">
+                        <h3 class="card-title">Online Store Overview</h3>
+                        <div class="card-tools">
+                            <a href="#" class="btn btn-sm btn-tool">
+                                <i class="fas fa-download"></i>
+                            </a>
+                            <a href="#" class="btn btn-sm btn-tool">
+                                <i class="fas fa-bars"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
+                            <p class="text-success text-xl">
+                                <i class="ion ion-ios-refresh-empty"></i>
+                            </p>
+                            <p class="d-flex flex-column text-right">
+                                <span class="font-weight-bold">
+                                    <i class="ion ion-android-arrow-up text-success"></i> 12%
+                                </span>
+                                <span class="text-muted">CONVERSION RATE</span>
+                            </p>
+                        </div>
+
+                        <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
+                            <p class="text-warning text-xl">
+                                <i class="ion ion-ios-cart-outline"></i>
+                            </p>
+                            <p class="d-flex flex-column text-right">
+                                <span class="font-weight-bold">
+                                    <i class="ion ion-android-arrow-up text-warning"></i> 0.8%
+                                </span>
+                                <span class="text-muted">SALES RATE</span>
+                            </p>
+                        </div>
+
+                        <div class="d-flex justify-content-between align-items-center mb-0">
+                            <p class="text-danger text-xl">
+                                <i class="ion ion-ios-people-outline"></i>
+                            </p>
+                            <p class="d-flex flex-column text-right">
+                                <span class="font-weight-bold">
+                                    <i class="ion ion-android-arrow-down text-danger"></i> 1%
+                                </span>
+                                <span class="text-muted">REGISTRATION RATE</span>
+                            </p>
+                        </div>
+
                     </div>
                 </div>
             </div>
-
-            <div class="card">
-                <div class="card-header border-0">
-                    <h3 class="card-title">Online Store Overview</h3>
-                    <div class="card-tools">
-                        <a href="#" class="btn btn-sm btn-tool">
-                            <i class="fas fa-download"></i>
-                        </a>
-                        <a href="#" class="btn btn-sm btn-tool">
-                            <i class="fas fa-bars"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
-                        <p class="text-success text-xl">
-                            <i class="ion ion-ios-refresh-empty"></i>
-                        </p>
-                        <p class="d-flex flex-column text-right">
-                            <span class="font-weight-bold">
-                                <i class="ion ion-android-arrow-up text-success"></i> 12%
-                            </span>
-                            <span class="text-muted">CONVERSION RATE</span>
-                        </p>
-                    </div>
-
-                    <div class="d-flex justify-content-between align-items-center border-bottom mb-3">
-                        <p class="text-warning text-xl">
-                            <i class="ion ion-ios-cart-outline"></i>
-                        </p>
-                        <p class="d-flex flex-column text-right">
-                            <span class="font-weight-bold">
-                                <i class="ion ion-android-arrow-up text-warning"></i> 0.8%
-                            </span>
-                            <span class="text-muted">SALES RATE</span>
-                        </p>
-                    </div>
-
-                    <div class="d-flex justify-content-between align-items-center mb-0">
-                        <p class="text-danger text-xl">
-                            <i class="ion ion-ios-people-outline"></i>
-                        </p>
-                        <p class="d-flex flex-column text-right">
-                            <span class="font-weight-bold">
-                                <i class="ion ion-android-arrow-down text-danger"></i> 1%
-                            </span>
-                            <span class="text-muted">REGISTRATION RATE</span>
-                        </p>
-                    </div>
-
-                </div>
-            </div>
         </div>
-
-        <div class="students">
-            <h2>Students</h2>
-            <p>Year 2023</p>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>STATUS</th>
-                        <th>Total Students Paid</th>
-                        <th>Total Students Unpaid</th>
-                        <th>Students with Paid MedicShare</th>
-                        <th>Students with Unpaid MedicShare</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>100</td>
-                        <td>75</td>
-                        <td>102</td>
-                        <td>50</td>
-                        <td>502</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-
-        <div class="monthly-data">
-            <h2>Jan Feb Mar Jun Jul Aug Sep Oct Nov Dec</h2>
-        </div>
-
-        <div class="view-all">
-            <a href="#">View All</a>
-        </div>
-    </div>
+    </section>
 @endsection
